9884823bdb3882321338925550d72d942d8b0716
Scott Bromander <scott@primeacademy.io>
2016-01-13 15:41:53 -0600

    small update

--- client/scripts/controllers/someController.js
+++ client/scripts/controllers/someController.js
@@ -1,10 +1,12 @@
 myApp.controller('SomeController', ['$scope', function($scope){
-    console.log("Some Controller");
+    //Create the Character (user) on the $scope.
+    $scope.user = {};
 
-    var user = {};
-
-    $scope.someAwesomeFunction = function(submittedUser){
-        console.log("HERE IS THE USER", submittedUser)
+    $scope.someAwesomeFunction = function(){
+        //Use the character information when the form is 'submitted'
+        console.log("HERE IS THE USER", $scope.user);
+        //Clear the character out now that you have hypothetically passed the character (user) along
+        $scope.user = {};
     };
 }]);
 
